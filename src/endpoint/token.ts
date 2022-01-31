a00587f572005e86c5707a68afb2274b19771043
Alex Castillo <alex@neurosity.co>
2022-01-30 18:01:20 -0800

    FEAT: add cors support for /token endpoint

--- src/endpoint/token.ts
+++ src/endpoint/token.ts
@@ -3,38 +3,46 @@ import {
   DefaultClientCredentialFetcherProvider,
   TokenEndpoint,
 } from "oauth2-nodejs";
+import * as cors from "cors";
+
 import { RequestWrapper } from "../models";
 import { CustomGrantHandlerProvider } from "../granttype";
 import { CloudFirestoreDataHandlerFactory } from "../data";
 
+const corsHandler = cors({ origin: true });
+
 export function token() {
   return functions.https.onRequest(async (req, resp) => {
-    if (req.method === "POST") {
-      const request = new RequestWrapper(req);
-      const tokenEndpoint = new TokenEndpoint();
-      const clientCredentialFetcherProvider = new DefaultClientCredentialFetcherProvider();
+    corsHandler(req, resp, async () => {
+      if (req.method === "POST") {
+        const request = new RequestWrapper(req);
+        const tokenEndpoint = new TokenEndpoint();
+        const clientCredentialFetcherProvider =
+          new DefaultClientCredentialFetcherProvider();
 
-      tokenEndpoint.grantHandlerProvider = new CustomGrantHandlerProvider(
-        clientCredentialFetcherProvider
-      );
-      tokenEndpoint.clientCredentialFetcherProvider = clientCredentialFetcherProvider;
-      tokenEndpoint.dataHandlerFactory = new CloudFirestoreDataHandlerFactory();
-
-      try {
-        const tokenEndpointResponse = await tokenEndpoint.handleRequest(
-          request
+        tokenEndpoint.grantHandlerProvider = new CustomGrantHandlerProvider(
+          clientCredentialFetcherProvider
         );
+        tokenEndpoint.clientCredentialFetcherProvider =
+          clientCredentialFetcherProvider;
+        tokenEndpoint.dataHandlerFactory =
+          new CloudFirestoreDataHandlerFactory();
 
-        resp.set("Content-Type", "application/json; charset=UTF-8");
-        resp
-          .status(tokenEndpointResponse.code)
-          .send(tokenEndpointResponse.body);
-      } catch (e) {
-        console.error(e);
-        resp.status(500).send(e.toString());
+        try {
+          const tokenEndpointResponse = await tokenEndpoint.handleRequest(
+            request
+          );
+
+          resp
+            .status(tokenEndpointResponse.code)
+            .json(tokenEndpointResponse.body);
+        } catch (e) {
+          console.error(e);
+          resp.status(500).send(e.toString());
+        }
+      } else {
+        resp.status(405).send("Method not allowed");
       }
-    } else {
-      resp.status(405).send("Method not allowed");
-    }
+    });
   });
 }
