d757026cee8cfe8ae6985eea525f3e12c8d3ea9d
Alex Castillo <alex@neurosity.co>
2022-01-30 18:02:02 -0800

    FEAT: add cors support, response_type and redirect param to /consent endpoint

--- src/endpoint/authorize.ts
+++ src/endpoint/authorize.ts
@@ -1,6 +1,7 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
 import * as ejs from "ejs";
+import * as cors from "cors";
 import { RequestWrapper } from "../models";
 import { AuthorizationEndpoint } from "oauth2-nodejs";
 import {
@@ -12,19 +13,22 @@ import { Configuration, Crypto, Navigation, processConsent } from "../utils";
 
 const authorizeApp = express();
 
+authorizeApp.use(cors({ origin: true }));
+
 authorizeApp.get("/entry", async (req, resp) => {
   const request = new RequestWrapper(req);
   const authorizationEndpoint = new AuthorizationEndpoint();
 
-  authorizationEndpoint.dataHandlerFactory = new CloudFirestoreDataHandlerFactory();
+  authorizationEndpoint.dataHandlerFactory =
+    new CloudFirestoreDataHandlerFactory();
   authorizationEndpoint.allowedResponseTypes = ["code", "token"];
 
   try {
-    const authorizationEndpointResponse = await authorizationEndpoint.handleRequest(
-      request
-    );
+    const authorizationEndpointResponse =
+      await authorizationEndpoint.handleRequest(request);
 
     if (authorizationEndpointResponse.isSuccess()) {
+      const redirect = request.getParameter("redirect") ?? "true";
       const authToken: { [key: string]: string | number } = {
         client_id: request.getParameter("client_id")!,
         redirect_uri: request.getParameter("redirect_uri")!,
@@ -41,14 +45,21 @@ authorizeApp.get("/entry", async (req, resp) => {
 
       const authTokenString = Crypto.encrypt(JSON.stringify(authToken));
 
+      const url = Navigation.buildUrl("/authentication/", {
+        auth_token: authTokenString,
+      });
+
+      if (redirect === "false") {
+        resp.status(200).json({ ok: true, url });
+        return;
+      }
+
       Navigation.redirect(resp, "/authentication/", {
         auth_token: authTokenString,
       });
     } else {
       const error = authorizationEndpointResponse.error;
-
-      resp.set("Content-Type", "application/json; charset=UTF-8");
-      resp.status(error.code).send(error.toJson());
+      resp.status(error.code).json(error.toJson());
     }
   } catch (e) {
     console.error(e);
@@ -59,6 +70,7 @@ authorizeApp.get("/entry", async (req, resp) => {
 
 authorizeApp.get("/consent", async (req, resp) => {
   const request = new RequestWrapper(req);
+  const responseType = request.getParameter("response_type") ?? "page";
   const encryptedAuthToken = request.getParameter("auth_token")!;
   const authToken = JSON.parse(Crypto.decrypt(encryptedAuthToken));
   const client = await CloudFirestoreClients.fetch(authToken["client_id"]);
@@ -66,15 +78,22 @@ authorizeApp.get("/consent", async (req, resp) => {
   const scopes = await CloudFirestoreScopes.fetch();
   const consentViewTemplate = Configuration.instance.view_consent_template;
 
+  const payload = {
+    scope: authToken["scope"],
+    encryptedAuthToken,
+    encryptedUserId,
+    scopes,
+    providerName: client!["providerName"],
+  };
+
+  if (responseType === "raw") {
+    resp.status(200).json(payload);
+    return;
+  }
+
   try {
     const template = await consentViewTemplate.provide();
-    const html = ejs.render(template, {
-      scope: authToken["scope"],
-      encryptedAuthToken,
-      encryptedUserId,
-      scopes,
-      providerName: client!["providerName"],
-    });
+    const html = ejs.render(template, payload);
 
     resp.status(200).send(html);
   } catch (e) {
@@ -85,17 +104,22 @@ authorizeApp.get("/consent", async (req, resp) => {
 
 authorizeApp.post("/consent", async (req, resp) => {
   const requestWrapper = new RequestWrapper(req);
+  const redirect = requestWrapper.getParameter("redirect") ?? "true";
   const encryptedAuthToken = requestWrapper.getParameter("auth_token")!;
   const authToken = JSON.parse(Crypto.decrypt(encryptedAuthToken));
   const encryptedUserId = requestWrapper.getParameter("user_id")!;
   const userId = Crypto.decrypt(encryptedUserId);
   const action = requestWrapper.getParameter("action");
 
-  return processConsent(resp, {
-    action,
-    authToken,
-    userId,
-  });
+  return processConsent(
+    resp,
+    {
+      action,
+      authToken,
+      userId,
+    },
+    { redirect: redirect === "true" }
+  );
 });
 
 export function authorize() {
